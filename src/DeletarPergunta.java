3a2f24ada168144e83c367dd0ad7885cd2e8fc57
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-11-12 22:35:11 -0300

    Função 4

--- src/DeletarPergunta.java
+++ src/DeletarPergunta.java
@@ -1,2 +1,68 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class DeletarPergunta {
+    public void executar (){
+        boolean isDeletado = false;
+        Scanner sc = new Scanner(System.in);
+        File file = new File("C:\\Users\\ispit\\Documents\\Foco total\\Java\\Sistema de Cadastros\\Perguntas\\formulário.txt");
+        int numPergunta = 1;
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)){
+            String linha;
+            while ((linha = br.readLine()) != null){
+                System.out.println(numPergunta+" - "+linha);// retorna null quando chega ao fim do arquivo
+                numPergunta++;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        while (!isDeletado){
+            System.out.println("Digite o número da pergunta que deseja excluir: ");
+            int deleteLinha = sc.nextInt();
+            if (deleteLinha <= 4){
+                System.out.println("Apenas as perguntas 5 em diante podem ser excluídas, tente novamente");
+            } else{
+                String tempFile = "temp.txt";
+                String pathArquivo = "C:\\Users\\ispit\\Documents\\Foco total\\Java\\Sistema de Cadastros\\Perguntas\\formulário.txt";
+                File arquivoAntigo = new File(pathArquivo);
+                File arquivoNovo = new File(tempFile);
+                int linha = 0;
+                String linhaAtual;
+
+                try{
+                    FileWriter fw = new FileWriter(tempFile, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter pw = new PrintWriter(bw);
+
+                    FileReader fr = new FileReader(pathArquivo);
+                    BufferedReader br = new BufferedReader(fr);
+
+                    while ((linhaAtual = br.readLine()) != null){
+                        linha++;
+
+                        if (deleteLinha != linha){
+                            pw.println(linhaAtual);
+                        }
+
+                    }
+                    pw.flush();
+                    pw.close();
+                    fr.close();
+                    br.close();
+                    bw.close();
+                    fw.close();
+
+                    arquivoAntigo.delete();
+                    File dump = new File(pathArquivo);
+                    arquivoNovo.renameTo(dump);
+                    System.out.println("Pergunta deletada com sucesso");
+                    isDeletado = true;
+                }catch (Exception e){
+                    System.out.println("Erro ao encontrar a pergunta escolhida");
+                }
+            }
+        }
+
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -4,37 +4,40 @@ public class Menu {
     CadastroUsuario cadastroUsuario = new CadastroUsuario();
     ListarUsuarios listarUsuarios = new ListarUsuarios();
     CadastroPergunta cadastroPergunta = new CadastroPergunta();
+    DeletarPergunta deletarPergunta = new DeletarPergunta();
 
     public void execute(){
         System.out.println("=============================================================");
         System.out.println("Digite a opção que deseja: ");
-        System.out.println("1 - Cadastrar o usuário\n" +
-                "2 - Listar todos usuários cadastrados\n" +
-                "3 - Cadastrar nova pergunta no formulário\n" +
-                "4 - Deletar pergunta do formulário\n" +
-                "5 - Pesquisar usuário por nome ou idade ou email");
+        System.out.println("""
+                1 - Cadastrar o usuário
+                2 - Listar todos usuários cadastrados
+                3 - Cadastrar nova pergunta no formulário
+                4 - Deletar pergunta do formulário
+                5 - Pesquisar usuário por nome ou idade ou email""");
         System.out.println("=============================================================");
 
         Scanner scannerMenu = new Scanner(System.in);
         int opc = scannerMenu.nextInt();
         switch (opc){
             case 1:
-                System.out.println("1 - Cadastrar o usuário");
+                System.out.println("1 - Cadastrar o usuário\n");
                 cadastroUsuario.executar();
                 break;
             case 2:
-                System.out.println("2 - Listar todos usuários cadastrados");
+                System.out.println("2 - Listar todos usuários cadastrados\n");
                 listarUsuarios.executar();
                 break;
             case 3:
-                System.out.println("3 - Cadastrar nova pergunta no formulário");
+                System.out.println("3 - Cadastrar nova pergunta no formulário\n");
                 cadastroPergunta.executar();
                 break;
             case 4:
-                System.out.println("4 - Deletar pergunta do formulário");
+                System.out.println("4 - Deletar pergunta do formulário\n");
+                deletarPergunta.executar();
                 break;
             case 5:
-                System.out.println("5 - Pesquisar usuário por nome ou idade ou email");
+                System.out.println("5 - Pesquisar usuário por nome ou idade ou email\n");
                 break;
             default:
                 System.out.println("Opção inválida. Encerrando aplicação...");
